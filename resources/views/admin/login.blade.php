3c728a9a4fe333264b4b6e836fca966b3f1e1c9f
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-18 22:20:33 +0600

    link

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -64,12 +64,12 @@
 		</div>
 		<!-- ./wrapper -->
 		<!-- jQuery -->
-		<script src="plugins/jquery/jquery.min.js"></script>
+		<script src="{{ asset('admin-assets/plugins/jquery/jquery.min.js') }}"></script>
 		<!-- Bootstrap 4 -->
-		<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="{{ asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 		<!-- AdminLTE App -->
-		<script src="js/adminlte.min.js"></script>
+		<script src="{{ asset('admin-assets/js/adminlte.min.js') }}"></script>
 		<!-- AdminLTE for demo purposes -->
-		<script src="js/demo.js"></script>
+		<script src="{{ asset('admin-assets/js/demo.js') }}"></script>
 	</body>
 </html>
