ad3340a5328521b65c4e8a9df245dba2c8fa1e73
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-22 00:39:51 +0600

    m

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -37,7 +37,7 @@
 					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link p-0 pr-3" data-toggle="dropdown" href="#">
-							<img src="img/avatar5.png" class='img-circle elevation-2' width="40" height="40" alt="">
+							<img src="{{ asset('admin-assets/img/avatar5.png') }}" class='img-circle elevation-2' width="40" height="40" alt="">
 						</a>
 						<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3">
 							<h4 class="h4 mb-0"><strong>Mohit Singh</strong></h4>
