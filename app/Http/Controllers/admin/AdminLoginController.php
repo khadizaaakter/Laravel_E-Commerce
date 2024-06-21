5895f60f914e97cb21dca30c6617904d1e2621bb
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-21 22:51:50 +0600

    auth

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Cache\RedisTagSet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -22,18 +23,27 @@ class AdminLoginController extends Controller
         ]);
 
         if ($validator->passes()) {
-            if (Auth::guard('admin')->attempt(['email' => $request->email, 
-            'password' => $request->password], $request->get('remember')))
-            {
-                return redirect()->route('admin.dashboard');
-            }else{
+            if (Auth::guard('admin')->attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ], $request->get('remember'))) {
+
+                $admin = Auth::guard('admin')->user();
+                if ($admin->role == 2) {
+                    return redirect()->route('admin.dashboard');
+                } else {
+                    Auth::guard('admin')->logout();
+                    return redirect()->route('admin.login')->with('error', 'You can not access admin panel.');
+                }
+            } else {
                 return redirect()->route('admin.login')->with('error', 'Either Email/Password is incorrect.');
             }
-            
         } else {
             return redirect()->route('admin.login')
                 ->withErrors($validator)
                 ->withInput($request->only('email'));
         }
     }
+
+
 }
--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
         echo 'welcome';
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
     }
 }
