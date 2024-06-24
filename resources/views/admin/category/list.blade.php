b62d7a40b33c88d9b32a7192716e0970f52e952c
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-24 09:38:16 +0600

    j

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -32,7 +32,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body table-responsive   p-0">
+                <div class="card-body table-responsive p-0">
                     <table class="table table-hover text-nowrap">
                         <thead>
                             <tr>
