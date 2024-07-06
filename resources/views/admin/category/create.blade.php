c17bbc23cde20b59ad21fbd21adfa1020f40c39e
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-07-06 22:25:07 +0600

    route

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -8,7 +8,7 @@
                     <h1>Create Category</h1>
                 </div>
                 <div class="col-sm-6 text-right">
-                    <a href="categories.html" class="btn btn-primary">Back</a>
+                    <a href="{{ route('categories.index') }}" class="btn btn-primary">Back</a>
                 </div>
             </div>
         </div>
