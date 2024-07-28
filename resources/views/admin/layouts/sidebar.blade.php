cc0fc5482c9a43b83caa0f4302b6d5128e42f77d
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-07-28 21:04:47 +0600

    l

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -68,9 +68,6 @@
                         <p>Discount</p>
                     </a>
                 </li>
-
-
-                
                 <li class="nav-item">
                     <a href="users.html" class="nav-link">
                         <i class="nav-icon  fas fa-users"></i>
