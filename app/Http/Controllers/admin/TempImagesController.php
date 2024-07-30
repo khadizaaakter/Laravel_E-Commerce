b0ef201e216dc4de983802cff967fd2f3646a812
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-07-30 12:27:40 +0600

    migration

--- app/Http/Controllers/admin/TempImagesController.php
+++ app/Http/Controllers/admin/TempImagesController.php
@@ -9,6 +9,15 @@ class TempImagesController extends Controller
 {
     public function create(Request $request)
     {
+        $image = $request->image;
         
+        if(!empty($image)){
+            $ext = $image->getClientOriginalExtension();
+            $newName = time().'.'.$ext;
+
+            $tempImage = new TempImage();
+            $tempImage->
+        
+        }
     }
 }
